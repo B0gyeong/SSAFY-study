390aee1c7fab68b81381b0e8dd69fdaf57afdfed
Jiwoo Kim <jiwoo.kim@stonybrook.edu>
2026-02-07 04:22:06 +0900

    password 성공

--- 0205/jiwoo/SolutionPwd.java
+++ 0205/jiwoo/SolutionPwd.java
@@ -27,36 +27,23 @@ public class SolutionPwd{
             int N = Integer.parseInt(st.nextToken());
             String inputStr = st.nextToken();
 
-            char[] input = new char[inputStr.length()];
-            int[] exclude = new int[input.length];
-            Arrays.fill(exclude, -1);
-            int exidx = 0;
-            int checked = 0; //already checked to exclude
-
-            char[] pwd = new char[input.length - (exclude.length / 2)];
-
-            for(int i = 1; i < input.length; i++){
-                char prev = input[i - 1];
-                char curr = input[i];
-                if(prev == curr){
-                    System.out.println("curr: " + curr);
-                    exclude[exidx++] = i - 1;
-                    exclude[exidx++] = i;
-                    i = checkExclude(input, exclude, i, exidx, checked); //returns the index of next one to check
-                    checked = i - 1;
+            char[] stack = new char[N];
+            int top = -1;
+            for(int i = 0; i < N; i++){
+                if(top == -1){
+                    stack[++top] = inputStr.charAt(i);
+                }else{
+                    if(stack[top] == inputStr.charAt(i)){
+                        stack[top--] = 0; // null -> pop
+                    }else{
+                        stack[++top] = inputStr.charAt(i);
+                    }
                 }
             }
 
             System.out.print("#" + test_case + " ");
-            int pwdIdx = 0;
-            for(int i = 0; i < input.length; i++){
-                int target = i;
-                boolean excluded = IntStream.of(exclude).anyMatch(x -> x == target);
-                if(!excluded){
-                    pwd[pwdIdx] = input[i];
-                    System.out.print(pwdIdx);
-                    pwdIdx++;
-                }
+            for(int i = 0; i <= top; i++){
+                System.out.print(stack[i]);
             }
             System.out.println();
         }
