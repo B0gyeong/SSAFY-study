334a6dd7e3a40cd4b0fcd8b7416ca3c2ed457c93
김지우 <jiwooclover@gmail.com>
2026-02-10 15:48:33 +0900

    수영장

---
+++ 0210/jiwoo/Main.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static char[] ops = { '+', '-', '*', '/' };
+    static int[] opsCnt = new int[4];
+    static int[] numbers;
+    static ArrayList<ArrayList<Integer>> opPer;
+    static ArrayList<Integer> opTmp;
+    static int N;
+
+    public static void dfs(int n){
+        if(n == N - 1){
+            opPer.add(new ArrayList<>(opTmp));
+            return;
+        }
+
+        for(int i = 0; i < 4; i++){
+            if(opsCnt[i] > 0){
+                opsCnt[i]--;
+                opTmp.add(i);
+                dfs(n + 1);
+                opsCnt[i]++;
+                opTmp.remove(opTmp.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine().trim());
+        opPer = new ArrayList<ArrayList<Integer>>();
+        opTmp = new ArrayList<Integer>();
+        numbers = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        for(int i = 0; i < N; i++){
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        st = new StringTokenizer(br.readLine().trim());
+        for(int i = 0; i < 4; i++){
+            opsCnt[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0);
+
+        int curr = numbers[0];
+        int max = -1000000000;
+        int min = 1000000000;
+
+        for(ArrayList<Integer> opList : opPer){
+            for(int i = 0; i < N - 1; i++){
+                int opIdx = opList.get(i);
+                int num = numbers[i + 1];
+                if(opIdx == 0){
+                    curr += num;
+                }else if(opIdx == 1){
+                    curr -= num;
+                }else if(opIdx == 2){
+                    curr *= num;
+                }else{
+                    curr /= num;
+                }
+            }
+
+            max = Math.max(curr, max);
+            min = Math.min(curr, min);
+            curr = numbers[0];
+        }
+
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
